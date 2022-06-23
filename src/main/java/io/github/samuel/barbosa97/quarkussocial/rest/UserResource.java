2e5f2c3ee5558665455d990a72c964bdb19730a9
Samuel Barbosa <samuel.barbosa1997@hotmail.com>
2022-06-23 17:50:55 -0300

    Utilizando o PanacheEntity

--- src/main/java/io/github/samuel/barbosa97/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/samuel/barbosa97/quarkussocial/rest/UserResource.java
@@ -1,7 +1,11 @@
 package io.github.samuel.barbosa97.quarkussocial.rest;
 
+import io.github.samuel.barbosa97.quarkussocial.domain.model.User;
 import io.github.samuel.barbosa97.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,12 +16,21 @@ import javax.ws.rs.core.Response;
 public class UserResource {
 
     @POST
+    @Transactional
     public Response createUser( CreateUserRequest userRequest ) {
-        return Response.ok(userRequest).build();
+
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        user.persist();
+
+        return Response.ok(user).build();
     }
 
     @GET
     public Response listAllUsers() {
-        return Response.ok().build();
+        PanacheQuery<User> query = User.findAll();
+        return Response.ok(query.list()).build();
     }
 }
